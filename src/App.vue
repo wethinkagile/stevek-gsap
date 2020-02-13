7b5cc5626adc0ba7dfdd3aba64c4bef9bc4ee58f
SteveK <steve@meshfields.de>
2020-02-13 19:02:33 +0100

    non working commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,254 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to SteveK.Pro"/>
-  </div>
+  <component :is="state.view">
+		<h1>{{ state.view }}</h1>
+	</component>
+  <controls></controls>
+</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import Controls from './components/Controls.vue'
+import Page from './components/Page.vue'
+
+
+import { TweenMax, TimelineMax, Power4 } from 'gsap'
+
+// Transitions
+const fade = Vue.component('fade', {
+  template: '#page',
+	methods: {
+		enter(el, done) {
+			TweenMax.fromTo(el, 1, {
+				autoAlpha: 0,
+				scale: 1.5,
+			}, {
+				autoAlpha: 1,
+				scale: 1,
+				transformOrigin: '50% 50%',
+				ease: Power4.easeOut,
+				onComplete: done
+			});
+		},
+		leave(el, done) {
+			TweenMax.fromTo(el, 1, {
+				autoAlpha: 1,
+				scale: 1,
+			}, {
+				autoAlpha: 0,
+				scale: 0.8,
+				ease: Power4.easeOut,
+				onComplete: done
+			});
+		}
+	}
+})
+
+const slide = Vue.component('slide', {
+  template: '#page',
+	methods: {
+		enter(el, done) {
+			const tl = new TimelineMax({
+				onComplete: done
+			})
+
+			tl.set(el, {
+				x: window.innerWidth * 1.5,
+				scale: 0.8,
+				transformOrigin: '50% 50%'
+			})
+
+			tl.to(el, 0.5, {
+				x: 0,
+				ease: Power4.easeOut
+			});
+
+			tl.to(el, 1, {
+				scale: 1,
+				ease: Power4.easeOut
+			});
+		},
+		leave(el, done) {
+			TweenMax.fromTo(el, 1, {
+				autoAlpha: 1
+			}, {
+				autoAlpha: 0,
+				ease: Power4.easeOut,
+				onComplete: done
+			});
+		}
+	}
+})
+
+const slideUp = Vue.component('slideUp', {
+  template: '#page',
+	methods: {
+		enter(el, done) {
+			const tl = new TimelineMax({
+				onComplete: done
+			})
+
+			tl.set(el, {
+				y: window.innerWidth * 1.5,
+				scale: 0.8,
+				transformOrigin: '50% 50%'
+			})
+
+			tl.to(el, 0.5, {
+				y: 0,
+				ease: Power4.easeOut
+			});
+
+			tl.to(el, 1, {
+				scale: 1,
+				ease: Power4.easeOut
+			});
+		},
+		leave(el, done) {
+			TweenMax.to(el, 1, {
+				y: window.innerHeight * -1.5,
+				ease: Power4.easeOut,
+				onComplete: done
+			});
+		}
+	}
+})
+
+const zoom = Vue.component('zoom', {
+  template: '#page',
+	methods: {
+		enter(el, done) {
+			const tl = new TimelineMax({
+				onComplete: done
+			})
+
+			tl.set(el, {
+				autoAlpha: 0,
+				scale: 2,
+				transformOrigin: '50% 50%'
+			})
+
+			tl.to(el, 1, {
+				autoAlpha: 1,
+				scale: 1,
+				ease: Power4.easeOut
+			})
+		},
+		leave(el, done) {
+			TweenMax.to(el, 1, {
+				scale: 0,
+				ease: Power4.easeOut,
+				onComplete: done
+			});
+		}
+	}
+})
+
+const flipX = Vue.component('flipX', {
+  template: '#page',
+	methods: {
+		enter(el, done) {
+			const tl = new TimelineMax({
+				onComplete: done
+			})
+
+			tl.set(el, {
+				autoAlpha: 0,
+				rotationX: 90,
+				transformOrigin: '50% 50%'
+			})
+
+			tl.to(el, 1, {
+				autoAlpha: 1,
+				rotationX: 0,
+				ease: Power4.easeOut
+			})
+		},
+		leave(el, done) {
+			TweenMax.to(el, 1, {
+				scale: 0,
+				ease: Power4.easeOut,
+				onComplete: done
+			});
+		}
+	}
+})
+
+const flipY = Vue.component('flipY', {
+  template: '#page',
+	methods: {
+		enter(el, done) {
+			const tl = new TimelineMax({
+				onComplete: done
+			})
+
+			tl.set(el, {
+				autoAlpha: 0,
+				rotationY: 90,
+				transformOrigin: '50% 50%'
+			})
+
+			tl.to(el, 1, {
+				autoAlpha: 1,
+				rotationY: 0,
+				ease: Power4.easeOut
+			})
+		},
+		leave(el, done) {
+			TweenMax.to(el, 1, {
+				scale: 0,
+				ease: Power4.easeOut,
+				onComplete: done
+			});
+		}
+	}
+})
+
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Controls,
+    Page
   }
 }
+
 </script>
 
-<style>
+<style scoped>
+
+$color1: #461467;
+$color2: #ffba57;
+$color3: lighten(#ff7655, 20%);
+$color4: lighten(#00aca0, 10%);
+$color5: #8ed3c9;
+$color6: darken(#fcf5d8, 20%);
+
+* {
+	box-sizing: border-box;
+}
+
+body {
+	background: #202020;
+	font-size: 62.5%;
+}
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+	overflow: hidden;
+	position: absolute;
+	left: 0;
+	top: 0;
+	width: 100vw;
+	height: 100vh;
+	background: rgba(76,76,76,1);
+	background: -moz-linear-gradient(-45deg, rgba(76,76,76,1) 0%, rgba(43,43,43,0.74) 36%, rgba(28,28,28,0.5) 71%, rgba(19,19,19,0.29) 100%);
+	background: -webkit-gradient(left top, right bottom, color-stop(0%, rgba(76,76,76,1)), color-stop(36%, rgba(43,43,43,0.74)), color-stop(71%, rgba(28,28,28,0.5)), color-stop(100%, rgba(19,19,19,0.29)));
+	background: -webkit-linear-gradient(-45deg, rgba(76,76,76,1) 0%, rgba(43,43,43,0.74) 36%, rgba(28,28,28,0.5) 71%, rgba(19,19,19,0.29) 100%);
+	background: -o-linear-gradient(-45deg, rgba(76,76,76,1) 0%, rgba(43,43,43,0.74) 36%, rgba(28,28,28,0.5) 71%, rgba(19,19,19,0.29) 100%);
+	background: -ms-linear-gradient(-45deg, rgba(76,76,76,1) 0%, rgba(43,43,43,0.74) 36%, rgba(28,28,28,0.5) 71%, rgba(19,19,19,0.29) 100%);
+	background: linear-gradient(135deg, rgba(76,76,76,1) 0%, rgba(43,43,43,0.74) 36%, rgba(28,28,28,0.5) 71%, rgba(19,19,19,0.29) 100%);
+	filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#4c4c4c', endColorstr='#131313', GradientType=1 );
+	color: #fff;
 }
 </style>
